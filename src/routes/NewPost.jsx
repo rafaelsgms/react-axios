55b3be0f20acbe1ed80c03de529e314f89b08b68
Rafael Gomes <rafael-silva-gomes@hotmail.com>
2023-02-06 18:04:53 -0300

    implementando o método post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,10 +1,30 @@
+import blogFetch from '../axios/config'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './NewPost.css'
 
 const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async(e) => {
+    e.preventDefault();
+
+    const post = { title, body, userId: 1 };
+
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/")
+  };
+
   return (
     <div className='new-post'>
       <h2>Inserir Novo Post:</h2>
-      <form>
+      <form onSubmit={(e) => createPost(e)}>
         <div className='form-control'>
           <label htmlFor="title">Título:</label>
           <input
@@ -12,11 +32,17 @@ const NewPost = () => {
             name="title"
             placeholder='Digite o título'
             id='title'
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className='form-control'>
           <label htmlFor="body">Conteúdo:</label>
-          <textarea name="body" id="body" placeholder='Digite o conteúdo'></textarea>
+          <textarea
+            name="body"
+            id="body"
+            placeholder='Digite o conteúdo'
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
         </div>
         <input type="submit" value="Criar Post" className='btn' />
       </form>
