ce12770d64c2100ef942889010261ea863f4bbb9
Rafael Gomes <rafael-silva-gomes@hotmail.com>
2023-02-06 16:49:44 -0300

    inicializando projeto com axios e react router dom

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Home from './routes/Home'
+import NewPost from './routes/NewPost'
+
+import {createBrowserRouter, RouterProvider, Router } from 'react-router-dom'
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    element:  <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/new",
+        element: <NewPost/>
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
