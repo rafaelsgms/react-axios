c573acc7acf53dbf2dfec565232b1c28e9e572a3
Rafael Gomes <rafael-silva-gomes@hotmail.com>
2023-02-06 17:55:28 -0300

    criando a aba de new post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className='new-post'>
+      <h2>Inserir Novo Post:</h2>
+      <form>
+        <div className='form-control'>
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            placeholder='Digite o título'
+            id='title'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="body" id="body" placeholder='Digite o conteúdo'></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className='btn' />
+      </form>
+    </div>
   )
 }
 
