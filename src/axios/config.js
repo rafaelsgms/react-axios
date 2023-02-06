b247e47ce4dcebe9b40230a44f03ebc437aabb3e
Rafael Gomes <rafael-silva-gomes@hotmail.com>
2023-02-06 17:46:06 -0300

    alterando o axios

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const blogFetch = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/"
+    baseURL: "https://jsonplaceholder.typicode.com/",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default blogFetch;
