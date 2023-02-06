95b280b4cd03a49982beed79e4f69c3d35ac2abf
Rafael Gomes <rafael-silva-gomes@hotmail.com>
2023-02-06 17:42:05 -0300

    configurando o axios globalmente com baseurl

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import blogFetch from "../axios/config"
 import { useEffect, useState } from "react"
 import { Link } from 'react-router-dom'
 import './Home.css'
@@ -11,7 +11,7 @@ const Home = () => {
 
     try {
 
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      const response = await blogFetch.get("/posts")
 
       const data = response.data;
 
@@ -28,7 +28,7 @@ const Home = () => {
   }, [])
 
   return (
-    <div>
+    <div className="home">
       <h1>Últimos posts</h1>
       {posts.length === 0 ? (<p>Carregando...</p>) : (
         posts.map((post) => (
